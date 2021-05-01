f1aa507fcc7b141d622dc5dda21a5f3a3c631323
humayunahmed8 <humayunfarhad3@gmail.com>
2021-05-01 21:23:28 +0600

    If else statement with date function

--- if-else.js
+++ if-else.js
@@ -18,3 +18,34 @@ if(number == 0){
 }else{
     console.log(number + 'is odd number')
 }
+
+
+
+var mainDate = new Date('April 30, 2021');
+// console.log(days.getMonth() + 1); // Because getmonth() start from 0
+
+var days = mainDate.getDay();
+
+// 0 - sunday 
+// 1 - Monday
+// 2 - tuesday
+// 3 - wednesday 
+// 4 - thursday 
+// 5 - friday
+// 6 - saturday 
+
+if(days == 0) {
+    console.log('Sunday')
+}else if(days == 1){
+    console.log('Monday')
+}else if(days == 2){
+    console.log('Tuesday')
+}else if(days == 3){
+    console.log('Wednesday')
+}else if(days == 4){
+    console.log('Thursday')
+}else if(days == 5){
+    console.log('Friday')
+}else{
+    console.log('Saturday')
+}
